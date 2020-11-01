b689edd2ada4d66d5eb918533c162e615a4a4885
Tomás <tomasaurrecoechea@opendeusto.es>
2020-11-01 21:25:18 +0100

    Completada la clase de hechizo y añadido comprobante de nivel

--- src/clasesCriatura/Hechizo.java
+++ src/clasesCriatura/Hechizo.java
@@ -1,5 +1,66 @@
 package clasesCriatura;
 
 public class Hechizo {
+	private String spellName;
+	private int level;
+	private String school;
+	private String castingTime;
+	private int range;
+	private String components;
+	private String duration;
+	private String description;
+	
+	
+	public String getSpellName() {
+		return spellName;
+	}
+	public void setSpellName(String spellName) {
+		this.spellName = spellName;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		if (this.level == 1 || this.level == 2 || this.level == 3 || this.level == 4 || this.level == 5 || this.level == 6 || this.level == 7 || this.level == 8 || this.level == 9) { 
+		this.level = level;
+		}
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	public String getCastingTime() {
+		return castingTime;
+	}
+	public void setCastingTime(String castingTime) {
+		this.castingTime = castingTime;
+	}
+	public int getRange() {
+		return range;
+	}
+	public void setRange(int range) {
+		this.range = range;
+	}
+	public String getComponents() {
+		return components;
+	}
+	public void setComponents(String components) {
+		this.components = components;
+	}
+	public String getDuration() {
+		return duration;
+	}
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 
 }
--- src/mainAndWindow/Prueba.java
+++
@@ -1,7 +0,0 @@
-package mainAndWindow;
-
-public class Prueba {
-	//Esta es una clase de prueba
-	
-	boolean prueba = false;
-}
