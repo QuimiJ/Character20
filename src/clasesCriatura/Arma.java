18e9d6eb4558c2fe4e9edc7bd22ab1354cf08414
Tomás <tomasaurrecoechea@opendeusto.es>
2020-11-04 20:57:07 +0100

    Completadas las clases del paquete Criatura

--- src/clasesCriatura/Arma.java
+++ src/clasesCriatura/Arma.java
@@ -1,19 +1,103 @@
 package clasesCriatura;
 
 public class Arma {
-	String nombre;
+	private String nombre;
 	
 	//Las armas son simple o martial
+	private boolean marcial;
 	
-	boolean marcial;
-	String daño;
-	String tipo;
+	private String daño;
+	private String tipo;
 	
 	//Las propiedades son cosas como Finesse, light, heavy o two-handed
 	//llegan a acumularse hasta 4 con las ballestas
+	private String propiedad1;
+	private String propiedad2;
+	private String propiedad3;
+	private String propiedad4;
 	
-	String propiedad1;
-	String propiedad2;
-	String propiedad3;
-	String propiedad4;
+	
+	//Getters y Setters
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public boolean isMarcial() {
+		return marcial;
+	}
+	public void setMarcial(boolean marcial) {
+		this.marcial = marcial;
+	}
+	public String getDaño() {
+		return daño;
+	}
+	public void setDaño(String daño) {
+		this.daño = daño;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getPropiedad1() {
+		return propiedad1;
+	}
+	public void setPropiedad1(String propiedad1) {
+		this.propiedad1 = propiedad1;
+	}
+	public String getPropiedad2() {
+		return propiedad2;
+	}
+	public void setPropiedad2(String propiedad2) {
+		this.propiedad2 = propiedad2;
+	}
+	public String getPropiedad3() {
+		return propiedad3;
+	}
+	public void setPropiedad3(String propiedad3) {
+		this.propiedad3 = propiedad3;
+	}
+	public String getPropiedad4() {
+		return propiedad4;
+	}
+	public void setPropiedad4(String propiedad4) {
+		this.propiedad4 = propiedad4;
+	}
+	
+	//Constructor
+	public Arma(String nombre, boolean marcial, String daño, String tipo, String propiedad1, String propiedad2,
+			String propiedad3, String propiedad4) {
+		super();
+		this.nombre = nombre;
+		this.marcial = marcial;
+		this.daño = daño;
+		this.tipo = tipo;
+		this.propiedad1 = propiedad1;
+		this.propiedad2 = propiedad2;
+		this.propiedad3 = propiedad3;
+		this.propiedad4 = propiedad4;
+	}
+	
+	public Arma() {
+		super();
+		this.nombre = null;
+		this.marcial = false;
+		this.daño = null;
+		this.tipo = null;
+		this.propiedad1 = null;
+		this.propiedad2 = null;
+		this.propiedad3 = null;
+		this.propiedad4 = null;
+	}
+	
+	public String toString() {
+		return "Arma [nombre=" + nombre + ", marcial=" + marcial + ", daño=" + daño + ", tipo=" + tipo + ", propiedad1="
+				+ propiedad1 + ", propiedad2=" + propiedad2 + ", propiedad3=" + propiedad3 + ", propiedad4="
+				+ propiedad4 + "]";
+	}
+	
+	
 }
--- src/clasesCriatura/Habilidad.java
+++
@@ -1,5 +0,0 @@
-package clasesCriatura;
-
-public class Habilidad {
-
-}
