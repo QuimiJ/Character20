fe9fb023f34124f5e38c97fd5c08e912e1c54336
Tomás <tomasaurrecoechea@opendeusto.es>
2020-11-04 13:31:42 +0100

    Provisionalmente hechas Clase y Rasgo

---
+++ src/clasesCriatura/Clase.java
@@ -0,0 +1,8 @@
+package clasesCriatura;
+
+public class Clase {
+	String nombre;
+	String hitdie;
+	Rasgo[] rasgos;
+
+}
--- src/clasesCriatura/Rasgo.java
+++ src/clasesCriatura/Rasgo.java
@@ -1,5 +1,7 @@
 package clasesCriatura;
 
 public class Rasgo {
+	String contenido;
+	
 
 }
